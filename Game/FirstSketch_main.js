6c3116db24e135e1136fcba8e2b489467bef9eb1
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-10-11 17:14:53 +0200

    World bounds and camera follow (with zoom in and out) sketch

--- Game/FirstSketch_main.js
+++ Game/FirstSketch_main.js
@@ -4,7 +4,7 @@
 
 let config = {
     type: Phaser.AUTO,
-    width: 1280,
+    width: 1280,        //The FoV of our camera will be the width x height we write here
     height: 720,        //[HERE] We have to make it responsive!
     title: 'First Sketch',
     version: '1.0',
@@ -87,21 +87,24 @@ function create() {
     //Create level
     platforms = this.physics.add.staticGroup();
 
-    platforms.create(config.width / 2, config.height - 16, 'platform').setScale(config.width / 400, 1).refreshBody();
-    platforms.create(config.width / 2 - levelWide / 2, config.height / 2, 'platform').setScale(1 / 400 * 32, config.height / 32).refreshBody();
-    platforms.create(config.width / 2 + levelWide / 2, config.height / 2, 'platform').setScale(1 / 400 * 32, config.height / 32).refreshBody();
+    platforms.create(skySpr.width / 2 * scaledW, skySpr.height * scaledH - 16, 'platform').setScale(config.width / 400, 1).refreshBody();
+    // platforms.create(config.width / 2 - levelWide / 2, config.height / 2, 'platform').setScale(1 / 400 * 32, config.height / 32).refreshBody();
+    // platforms.create(config.width / 2 + levelWide / 2, config.height / 2, 'platform').setScale(1 / 400 * 32, config.height / 32).refreshBody();
 
     //Create player
     player = this.physics.add.sprite(skySpr.width / 2 * scaledW, skySpr.height * scaledH - 32 - 333 / 2, 'player').setScale(32 / 334, 32 / 333).refreshBody();
     player.setBounce(0.4, 0.2);
     player.setDrag(50, 0);
-    //player.setCollideWorldBounds(true);
+    player.setCollideWorldBounds(true);
 
     //Physics
     this.physics.add.collider(player, platforms);
 
-    //Camera follow
-    this.cameras.main.setBounds(config.width, 0, config.width, skySpr.height * scaledH); //First two parameters are top left and the other two are bottom right corners
+    //Camera follow and bounds
+    //this.cameras.main.setBounds(config.width, 0, config.width, skySpr.height * scaledH); //First two parameters are top left and the other two are bottom right corners
+    this.physics.world.setBounds(config.width, 0, config.width, skySpr.height * scaledH); //The world bounds define where the world colliders are (its like a box for the player/s)
+    this.cameras.main.setBounds(0, 0, skySpr.width * scaledW, skySpr.height * scaledH); //The camera will be able to move all around the map, and we'll change the size of the world and make zoom to vary the player/s FoV
+    this.cameras.main.scrollX = config.width; //scrollX is X coordinate of the top left corner of the FoV of our cam
 }
 
 function update() {
@@ -116,18 +119,18 @@ function update() {
         player.setVelocityX(-250);
 
     //Camera variation
-    this.cameras.main.scrollY = player.y - config.height / 2;
-    console.log("scrollX: " + this.cameras.main.scrollX);
-    console.log("scrollY: " + this.cameras.main.scrollY);
+    this.cameras.main.scrollY = player.y - config.height / 2; //scrollY is the Y coordinate top left corner of the FoV of our cam
 
     if (player.y < config.height * 6 && !zoomedOut) {
         zoomedOut = true;
-        this.cameras.main.setBounds(0, 0, skySpr.width * scaledW, skySpr.height * scaledH, true);
+        this.physics.world.setBounds(0, 0, skySpr.width * scaledW, skySpr.height * scaledH);
+        //this.cameras.main.setBounds(0, 0, skySpr.width * scaledW, skySpr.height * scaledH);
         this.cameras.main.zoomTo(1/3, 2000);
     }
     if (player.y < config.height * 3 && !zoomedIn) {
         zoomedIn = true;
-        //this.cameras.main.setBounds(config.width, 0, config.width, skySpr.height * scaledH, true);
+        this.physics.world.setBounds(config.width, 0, config.width, skySpr.height * scaledH);
+        //this.cameras.main.setBounds(config.width, 0, config.width, skySpr.height * scaledH);
         this.cameras.main.zoomTo(1, 2000);
     }
 }
